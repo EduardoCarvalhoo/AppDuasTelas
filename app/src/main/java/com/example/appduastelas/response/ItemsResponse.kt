8c07b901c418db130a6420fb4dbc15bab66ea6d4
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-25 21:44:49 -0300

    Criando itens para mapeamento de dados

---
+++ app/src/main/java/com/example/appduastelas/response/ItemsResponse.kt
@@ -0,0 +1,12 @@
+package com.example.appduastelas.response
+
+data class ItemsResponse(
+    val name: String?,
+    val quantity: String?,
+    val stock: String?,
+    val image_url: String?,
+    val price: String?,
+    val tax: String?,
+    val shipping: String?,
+    val description: String?
+)
