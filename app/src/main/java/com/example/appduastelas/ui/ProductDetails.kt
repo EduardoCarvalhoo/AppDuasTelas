c46df9027fd8f383cdd457ae528e5829122a2403
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-07-26 12:06:27 -0300

    Adicionando segunda tela para detalhes

--- app/src/main/java/com/example/appduastelas/ui/ProductDetails.kt
+++ app/src/main/java/com/example/appduastelas/ui/ProductDetails.kt
@@ -1,11 +1,12 @@
-package com.example.appduastelas
+package com.example.appduastelas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appduastelas.R
 
-class MainActivity : AppCompatActivity() {
+class ProductDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product_details)
     }
 }
